a09638ba36d8af832fa82df8fabc3d3cdced095e
Akash Panwar <akashpanwar9976@gmail.com>
2022-09-01 20:50:30 +0530

    fetching data from server

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,21 @@
-import React from "react";
+import React, { useEffect } from "react";
 import PokemonPage from "./PokemonPage";
 
+const fetchPokemons = async () => {
+  const res = await fetch("http://localhost:3001/db");
+
+  if (!res.ok) return;
+
+  const { pokemon: pokemons } = await res.json();
+
+  console.log(pokemons);
+};
+
 function App() {
+  useEffect(() => {
+    fetchPokemons();
+  }, []);
+
   return (
     <div className="App">
       <PokemonPage />
